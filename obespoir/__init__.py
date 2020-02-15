2d391368ae340d5c8d83b440548a6b27e9576cac
jamon <jamonhe1990@gmail.com>
2020-02-15 18:15:06 +0800

    添加打包功能

---
+++ obespoir/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+"""
+author = jamon
+"""
+
+version = "0.1.5"
--- setup.py
+++ setup.py
@@ -3,11 +3,33 @@
 author = jamon
 """
 
+import sys
+
 from setuptools import setup, find_packages
+
+
+if 2 > len(sys.argv):
+    print("param error, please input 'python3 setup.py sdist bdist_wheel'")
+    exit(0)
+
+
+try:
+    # Use setuptools if available, for install_requires (among other things).
+    import setuptools
+    from setuptools import setup
+except ImportError:
+    setuptools = None
+    from distutils.core import setup
+
+
+with open("obespoir/__init__.py") as f:
+    ns = {}
+    exec(f.read(), ns)
+    version = ns["version"]
 
 setup(
     name="obEspoir",
-    version="0.1.1",
+    version=version,
     keywords=("pip", "obEspoir", "game", "server", "frame", "distribute"),
     description="a distribute game frame",
     long_description="a distribute game frame",
